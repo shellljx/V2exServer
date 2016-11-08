92835195f6c60bbd91df402415d02b31754566f7
shell <shellljx@gmail.com>
2016-11-08 11:03:37 +0800

    commit

--- v2ex.js
+++ v2ex.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var cheerio = require('cheerio');
-var superagent = require('superagent');
+var superagent = require('superagent').agent();
 const HOST = 'https://www.v2ex.com';
 const LOGIN_PATH = HOST+'/signin';
 const PAGE_SIZE = 50;
 
 var app = express();
+app.use(require('body-parser')());
 
 app.get('/',function(req,res,next){
 	superagent.get(HOST)
@@ -36,7 +37,11 @@ app.get('/',function(req,res,next){
 	});
 });
 
-app.get('/signin',function(req,res,next){
+app.post('/signin',function(req,res,next){
+
+	var name = req.body.name;
+	var password = req.body.password;
+
 	superagent.get(LOGIN_PATH)
 	.end(function(err,sres){
 		if (err) {
@@ -44,6 +49,31 @@ app.get('/signin',function(req,res,next){
 		}
 		var $ = cheerio.load(sres.text);
 		var once = $('input[name=once]').attr('value');
+		var input_name = $('input[placeholder=用户名或电子邮箱地址]').attr('name');
+		var input_psw = $('input[type=password]').attr('name');
+		var cookies = sres.header['set-cookie'];
+		var body = {};
+		body[input_name] = '1057645164@qq.com';
+		body[input_psw] = '149162536max';
+		body['once'] = once;
+		body['next'] = '/';
+
+		console.log('开始执行post方法');
+		superagent.post(LOGIN_PATH)
+    	.type('form')
+    	.set('cookie','PB3_SESSION="";V2EX_LANG=zhcn;V2EX_TAB=""')
+    	.set('referer','https://www.v2ex.com/signin')
+    	.send(body)
+    	.end(function(err,loginRes){
+    		var loginCookie = 'A2="2|1:0|10:1478570076|2:A2|56:MGVhMTU2YWM5ZDFmMzM4NjRiYzE5NDgzMjAyMGMxNDRiOGVjNWM5OQ==|f4773efc3c549ff1441b1171b89e8f697f050a4bf53a15d39fb62c6ac1d18eb2"; Domain=.v2ex.com; expires=Wed, 08 Nov 2017 01:54:36 GMT; httponly; Path=/';
+    		console.log(loginCookie);
+    		res.send(loginRes);
+    		// superagent.get(HOST+'/recent')
+    		// .set('cookie',loginCookie)
+    		// .end(function(err,recentRes){
+    		// 	res.send(recentRes);
+    		// })
+  		});
 	});
 });
 
